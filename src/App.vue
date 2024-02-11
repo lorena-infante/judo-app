877b691578e6aaf9309938921fefa89b131cc8a3
Lorena Infante <m.lorenainfanter@gmail.com>
2024-02-10 23:32:19 -0300

    first config electron+vite+TS+vue

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>💖 Hello World!</h1>
+        <p>Welcome to your Electron application.</p>
+        <button class="btn btn-success">Hola Bootstrap!</button>
+    </div>
+  </template>
+  
+  <script setup>
+  console.log('👋 This message is being logged by "App.vue", included via Vite');
+  </script>
